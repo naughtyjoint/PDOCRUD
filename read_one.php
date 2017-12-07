8130c50a9478d2bbf929ca33a3a11ab97ca61950
naughtyjoint <wateleee@gmail.com>
2017-12-07 18:10:46 +0800

    Initial Commit

---
+++ read_one.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PDO - Read One Record - PHP CRUD Tutorial</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="libs/bootstrap/dist/css/bootstrap.min.css" />
+
+    <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+
+<!-- container -->
+<div class="container">
+
+    <div class="page-header">
+        <h1>Read Product</h1>
+    </div>
+
+    <!-- dynamic content will be here -->
+
+</div> <!-- end .container -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="libs/js/jquery-3.0.0.min.js"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="libs/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
